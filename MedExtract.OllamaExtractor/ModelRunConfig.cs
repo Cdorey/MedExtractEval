3c7d1ee27394e8d95016a3b1e7ddd9f51e36680b
bsense <bsense@live.com>
2026-03-02 19:53:56 +0800

    批量参数运行

---
+++ MedExtract.OllamaExtractor/ModelRunConfig.cs
@@ -0,0 +1,3 @@
+﻿
+//帮我生成一个小的record，包括上面我注释掉的那几个变量和它们的类型，方便传参
+record ModelRunConfig(string ModelName, string VersionTag, float Temperature, double TopP, string Provider, bool ThinkEnable);
--- MedExtract.OllamaExtractor/Program.cs
+++ MedExtract.OllamaExtractor/Program.cs
@@ -17,48 +17,63 @@ var sysPromtDict = new Dictionary<string, string>
     { "ExamCoronaryCTA","对冠脉CTA报告的诊断结论进行分类，你只回复一个True/False，存在狭窄或斑块为True，无该问题为False，如果是支架术后，认为是发生过狭窄，其他类型的异常不需要关注：" },
 };
 
-var defaultModelName = "ministral-3:14b";
-var defaultVersionTage = "4760c35aeb9d";
-var defaultTemperature = 0.15f;
-var defaultTopP = 0.90;
-var defaultProvider = "Ollama";
-var thinkEnable = false;
-
 var store = new ExtractionStore(ConnectionStringResolvers.ConsolePrompt());
-
 var experiments = await store.ListExperimentsAsync();
 
-foreach (var exp in experiments)
+ModelRunConfig[] modelConfigs = [
+    //new ModelRunConfig("qwen3-vl:30b","eda0be100877",0,0.95,"Ollama",false),
+    //new ModelRunConfig("qwen3-next:80b","b2ebb986e4e9",0,0.95,"Ollama",false),
+    new ModelRunConfig("qwen3:32b","030ee887880f",0,0.95,"Ollama",false),
+    new ModelRunConfig("qwen3:32b","030ee887880f",0.6f,0.95,"Ollama",false),
+    //new ModelRunConfig("qwen3:30b","ad815644918f",0,0.95,"Ollama",false),
+    new ModelRunConfig("qwen3:0.6b","7df6b6e09427",0,0.95,"Ollama",false),
+    new ModelRunConfig("qwen3:0.6b","7df6b6e09427",0.6f,0.95,"Ollama",false),
+    //new ModelRunConfig("ministral-3:3b","f04aa1c738f6",0,0.90,"Ollama",false),
+    //new ModelRunConfig("ministral-3:14b","4760c35aeb9d",0,0.90,"Ollama",false),
+    //new ModelRunConfig("gemma3:4b","a2af6cc3eb7f",0,0.95,"Ollama",false),
+    //new ModelRunConfig("gemma3:27b","a418f5838eaf",0,0.95,"Ollama",false),
+    ];
+
+foreach (var config in modelConfigs)
 {
-    if (sysPromtDict.Keys.Where(k => exp.Name.Contains(k)).FirstOrDefault() is string key)
+    await MainTreatmentLoop(config);
+}
+
+
+async Task MainTreatmentLoop(ModelRunConfig runConfig)
+{
+    foreach (var exp in experiments)
     {
-        var sysPrompt = sysPromtDict[key];
-        var modelConfig = await store.CreateOrGetModelConfigAsync(
-            modelName: defaultModelName,
-            versionTag: defaultVersionTage,
-            temperature: defaultTemperature,
-            topP: defaultTopP,
-            provider: defaultProvider,
-            promptTemplate: sysPrompt,
-            isDeterministic: false,
-            isThinking: thinkEnable);
-
-        var cases = await store.ListPendingCaseItemsAsync(exp, modelConfig);
-
-        foreach (var caseItem in cases)
+        if (sysPromtDict.Keys.Where(k => exp.Name.Contains(k)).FirstOrDefault() is string key)
         {
-            var ollamaTask = new OllamaTask(ollama, modelConfig.ModelName, modelConfig.PromptTemplate, caseItem.RawText, defaultTemperature, modelConfig.IsThinking);
-            var x = await ollamaTask.RunAsync();
-            var extraction = new OllamaExtraction(exp, caseItem, modelConfig);
-            extraction.FromChatRunResult(x);
+            var sysPrompt = sysPromtDict[key];
+            var modelConfig = await store.CreateOrGetModelConfigAsync(
+                modelName: runConfig.ModelName,
+                versionTag: runConfig.VersionTag,
+                temperature: runConfig.Temperature,
+                topP: runConfig.TopP,
+                provider: runConfig.Provider,
+                promptTemplate: sysPrompt,
+                isDeterministic: false,
+                isThinking: runConfig.ThinkEnable);
 
-            await store.AddModelExtractionAsync(extraction);
+            var cases = await store.ListPendingCaseItemsAsync(exp, modelConfig);
 
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine(x.ThinkingText);
-            Console.ResetColor();
-            Console.WriteLine(x.ContentText);
-            Console.WriteLine($"PromptTokens: {extraction.PromptTokens}, CompletionTokens: {extraction.CompletionTokens}, Latency: {extraction.Latency.TotalSeconds}s");
+            foreach (var caseItem in cases)
+            {
+                var ollamaTask = new OllamaTask(ollama, modelConfig.ModelName, modelConfig.PromptTemplate, caseItem.RawText, runConfig.Temperature, modelConfig.IsThinking);
+                var x = await ollamaTask.RunAsync();
+                var extraction = new OllamaExtraction(exp, caseItem, modelConfig);
+                extraction.FromChatRunResult(x);
+
+                await store.AddModelExtractionAsync(extraction);
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine(x.ThinkingText);
+                Console.ResetColor();
+                Console.WriteLine(x.ContentText);
+                Console.WriteLine($"PromptTokens: {extraction.PromptTokens}, CompletionTokens: {extraction.CompletionTokens}, Latency: {extraction.Latency.TotalSeconds}s");
+            }
         }
     }
 }
